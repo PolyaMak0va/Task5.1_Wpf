a340c925551054be275fe206a6ea01bdb7712ac0
Ксения Финаева <polya.makova@outlook.com>
2021-12-22 22:44:42 +0300

    Добавьте файлы проекта.

---
+++ Task5.1_Wpf/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task5._1_Wpf;
+
+namespace Example1_WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+
+            int newValue;
+            if (int.TryParse(fontSize, out newValue))
+            {
+                if (textBox != null)
+                {
+                    textBox.FontSize = newValue;
+                }
+            }
+        }
+
+        private void btnBold_Click(object sender, RoutedEventArgs e)
+        {
+            //int weightValue = 999;
+            //textBox.FontWeight = FontWeight.FromOpenTypeWeight(weightValue);
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void btnItalic_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void btnUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == TextDecorations.Underline)
+            {
+                textBox.TextDecorations = null;
+            }
+            else
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+        }
+
+        private void rb1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rb1.IsChecked == true)
+            {
+                if (textBox != null)
+                {
+                    textBox.Foreground = Brushes.Black;
+                }
+            }
+            else if (rb2.IsChecked == true)
+            {
+                if (textBox != null)
+                {
+                    textBox.Foreground = Brushes.Red;
+                }
+            }
+        }
+
+        private void open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void save_as_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF file (*.pdf)|*.pdf|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void print_Click(object sender, RoutedEventArgs e)
+        {
+            Print print = new Print();
+            print.WindowStyle = WindowStyle.ToolWindow;
+            print.Show();
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}

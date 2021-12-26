6a3332154ecf17ed137d7a39884972977884a3c2
Ксения Финаева <polya.makova@outlook.com>
2021-12-26 23:07:09 +0300

    fix

--- Task5.2_Wpf/MainWindow.xaml.cs
+++ Task5.2_Wpf/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -22,20 +23,14 @@ namespace Task5._2_Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SolidColorBrush SelectedColor { get; set; } = Brushes.Black;
+        public enum SelectedTools { LargeBrush = 0, Brush = 1, Pencil = 2 }
+        public int SelectedTool = 2;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        //private void BtnInk_Click(object sender, RoutedEventArgs e)
-        //{
-        //    inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
-
-        //    // Set the DefaultDrawingAttributes for a red pen.
-        //    inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
-        //    inkCanvas.DefaultDrawingAttributes.IsHighlighter = false;
-        //    inkCanvas.DefaultDrawingAttributes.Height = 2;
-        //}
         private void Open_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -45,68 +40,100 @@ namespace Task5._2_Wpf
             {
                 image.Source = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Absolute));
             }
+            //if (openFileDialog.ShowDialog() == true)
+            //{
+            //    string fileName = openFileDialog.FileName;
+            //    BitmapImage bitmap = new BitmapImage();
+            //    bitmap.BeginInit();
+            //    bitmap.UriSource = new Uri(fileName);
+            //    bitmap.EndInit();
+            //    image.Source = bitmap;
+            //}
         }
-
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            this.inkCanvas.EditingMode = InkCanvasEditingMode.None;
             SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" + "Portable Network Graphic (*.png)|*.png" };
-            saveFileDialog.Title = "Безымянный";
-            FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create);
 
-            RenderTargetBitmap bitmap = new RenderTargetBitmap((int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight,96,96,PixelFormats.Default);
-            bitmap.Render(inkCanvas);
-            inkCanvas.Strokes.Save(fs);
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fileName = saveFileDialog.FileName;
+
+                int margin = (int)this.inkCanvas.Margin.Left;
+                inkCanvas.Margin = new Thickness(0, 0, 0, 0);
+                int width = (int)this.inkCanvas.ActualWidth - margin;
+                int height = (int)inkCanvas.ActualHeight - margin;
+
+                RenderTargetBitmap bitmap =
+                    new RenderTargetBitmap(width, height, 0d, 0d, PixelFormats.Default);
+                inkCanvas.Measure(new Size((int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight));
+                inkCanvas.Arrange(new Rect(new Size((int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight)));
+                bitmap.Render(inkCanvas);
+
+                using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                {
+                    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                    encoder.Save(fs);
+                }
+            }
         }
-
-        //private void close_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string messageBoxText = "Вы хотите сохранить изменения?";
-        //    string caption = "Графический редактор";
-
-        //    MessageBoxButton button = MessageBoxButton.YesNoCancel;
-        //    MessageBoxImage icon = MessageBoxImage.Warning;
-        //    MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
-
-        //    switch (result)
-        //    {
-        //        case MessageBoxResult.Yes:
-        //            {
-        //                SaveFileDialog saveFileDialog = new SaveFileDialog();
-        //                saveFileDialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" + "Portable Network Graphic (*.png)|*.png";
-
-        //                if (saveFileDialog.ShowDialog() == true)
-        //                {
-        //                    File.WriteAllText(saveFileDialog.FileName, canvas.ima);
-        //                }
-        //                break;
-        //            }
-        //        case MessageBoxResult.No:
-        //            {
-        //                Application.Current.Shutdown();
-        //                break;
-        //            }
-        //        case MessageBoxResult.Cancel:
-        //            {
-        //                break;
-        //            }
-        //    }
-        //}
-
-        private void BtnEraseStroke_Checked(object sender, RoutedEventArgs e)
+        private void Сlose_Click(object sender, RoutedEventArgs e)
         {
-            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+            string messageBoxText = "Вы хотите сохранить изменения?";
+            string caption = "Графический редактор";
+
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    {
+                        SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" + "Portable Network Graphic (*.png)|*.png" };
+
+                        if (saveFileDialog.ShowDialog() == true)
+                        {
+                            string fileName = saveFileDialog.FileName;
+
+                            int margin = (int)this.inkCanvas.Margin.Left;
+                            inkCanvas.Margin = new Thickness(0, 0, 0, 0);
+                            int width = (int)this.inkCanvas.ActualWidth - margin;
+                            int height = (int)inkCanvas.ActualHeight - margin;
+
+                            RenderTargetBitmap bitmap =
+                                new RenderTargetBitmap(width, height, 0d, 0d, PixelFormats.Default);
+                            inkCanvas.Measure(new Size((int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight));
+                            inkCanvas.Arrange(new Rect(new Size((int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight)));
+                            bitmap.Render(inkCanvas);
+
+                            using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                            {
+                                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                                encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                                encoder.Save(fs);
+                            }
+                        }
+                        break;
+                    }
+                case MessageBoxResult.No:
+                    {
+                        Application.Current.Shutdown();
+                        break;
+                    }
+                case MessageBoxResult.Cancel:
+                    {
+                        break;
+                    }
+            }
         }
-
-
-        private void BtnInk_Click(object sender, RoutedEventArgs e)
+        private void Print_Click(object sender, RoutedEventArgs e)
         {
-            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
-
-            // Set the DefaultDrawingAttributes for a red pen.
-            inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
-            inkCanvas.DefaultDrawingAttributes.IsHighlighter = false;
-            inkCanvas.DefaultDrawingAttributes.Height = 2;
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(inkCanvas, "Распечатываем элемент Canvas");
+            }
         }
 
         private void Highlight_Click(object sender, RoutedEventArgs e)
@@ -117,70 +144,89 @@ namespace Task5._2_Wpf
             inkCanvas.DefaultDrawingAttributes.IsHighlighter = true;
             inkCanvas.DefaultDrawingAttributes.Height = 25;
         }
-
         private void BtnEraseByPoint_Click(object sender, RoutedEventArgs e)
         {
             inkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
         }
-
         private void BtnSelect_Click(object sender, RoutedEventArgs e)
         {
             this.inkCanvas.EditingMode = InkCanvasEditingMode.Select;
         }
-
-
-
-        //private void save_as_Click(object sender, RoutedEventArgs e)
-        //{
-        //    SaveFileDialog saveFileDialog = new SaveFileDialog();
-        //    saveFileDialog.Filter = "PDF file (*.pdf)|*.pdf|Все файлы (*.*)|*.*";
-
-        //    if (saveFileDialog.ShowDialog() == true)
-        //    {
-        //        File.WriteAllText(saveFileDialog.FileName, textBox.Text);
-        //    }
-        //}
-
-        private void print_Click(object sender, RoutedEventArgs e)
+        private void Cut_Click(object sender, RoutedEventArgs e)
         {
-            PrintDialog printDialog = new PrintDialog();
-            if (printDialog.ShowDialog() == true)
+            StrokeCollection selection = inkCanvas.GetSelectedStrokes();
+
+            if (selection.Count > 0)
             {
-                printDialog.PrintVisual(inkCanvas, "Распечатываем элемент Canvas");
+                foreach (Stroke stroke in selection)
+                {
+                    this.inkCanvas.CutSelection();
+                }
             }
         }
-
-        private void MainSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void Copy_Click(object sender, RoutedEventArgs e)
         {
-            inkCanvas.DefaultDrawingAttributes.Width = ((Slider)sender).Value; // одно из двух или MainInk=null или MainInk.DefaultDrawingAttributes=null
-            inkCanvas.DefaultDrawingAttributes.Height = ((Slider)sender).Value;
+            StrokeCollection selection = inkCanvas.GetSelectedStrokes();
+
+            if (selection.Count > 0)
+            {
+                foreach (Stroke stroke in selection)
+                {
+                    this.inkCanvas.CopySelection();
+                }
+            }
+        }
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.inkCanvas.CanPaste())
+            {
+                this.inkCanvas.Paste();
+            }
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas.Strokes.Clear();
+        }
+        public void BtnColorSelected(object sender, RoutedEventArgs e)
+        {
+            Button btnSelected = (Button)sender;
+            SelectedColor = (SolidColorBrush)btnSelected.Background;
+            RctColorSelected.Fill = SelectedColor;
+            strokeAttr.Color = SelectedColor.Color;
         }
 
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Pencil_Click(object sender, RoutedEventArgs e)
         {
-            StackPanel mainStackPanel = new StackPanel();
-            TextBox pasteTextBox = new TextBox();
-            Menu stackPanelMenu = new Menu();
-            MenuItem pasteMenuItem = new MenuItem();
+            Button btnSelected = (Button)sender;
+            SelectedTool = (int)SelectedTools.Pencil;
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
 
-            // Adding objects to the panel and the menu
-            stackPanelMenu.Items.Add(pasteMenuItem);
-            mainStackPanel.Children.Add(stackPanelMenu);
-            mainStackPanel.Children.Add(pasteTextBox);
-
-            // Setting the command to the Paste command
-            pasteMenuItem.Command = ApplicationCommands.Paste;
-
-            // Setting the command target to the TextBox
-            pasteMenuItem.CommandTarget = pasteTextBox;
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+            inkCanvas.DefaultDrawingAttributes.IsHighlighter = false;
+            strokeAttr.Width = 3;
+            strokeAttr.Height = 3;
         }
 
-        private void _Cut_Click(object sender, RoutedEventArgs e)
+        private void Brush_Click(object sender, RoutedEventArgs e)
         {
-            Button cutButton = new Button();
-            cutButton.Command = ApplicationCommands.Cut;
+            Button btnSelected = (Button)sender;
+            SelectedTool = (int)SelectedTools.Brush;
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+
+            inkCanvas.DefaultDrawingAttributes.IsHighlighter = false;
+            strokeAttr.Width = 10;
+            strokeAttr.Height = 10;
         }
 
+        private void LargeBrush_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnSelected = (Button)sender;
+            SelectedTool = (int)SelectedTools.LargeBrush;
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+
+            inkCanvas.DefaultDrawingAttributes.IsHighlighter = false;
+            strokeAttr.Width = 20;
+            strokeAttr.Height = 20;
+        }
     }
 }
